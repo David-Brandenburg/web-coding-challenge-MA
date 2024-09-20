cd22e71da4f2902bd0270c14a02a6a14561082cd
win/ubuntu <d-brandenburg@hotmail.de>
2024-09-20 19:58:33 +0200

    feat: style der Landingpage angepasst.

---
+++ description.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cube</title>
+</head>
+<body>
+    <nav>
+        <a href="login.php">Login</a>
+        <a href="index.php">Home</a>
+    </nav>
+    <main>
+        <h1>description</h1>
+    </main>
+    
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-$logos = json_decode(file_get_contents('logoList.json'), true);
+$jsonLogos = file_get_contents('logoList.json');
+$logos = json_decode($jsonLogos, true);
 ?>
 
 <!DOCTYPE html>
@@ -8,15 +9,33 @@ $logos = json_decode(file_get_contents('logoList.json'), true);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
     <title>Cube</title>
 </head>
 <body>
-    <nav>
+    <nav class="nav-container">
         <a href="Regi.php">Registration</a>
         <a href="manage.php">Management</a>
     </nav>
     <main>
-        <h1>Hello you!</h1>
+        <div class="landing-container">
+            <button onClick="window.location.href='description.php'" class="landing-card">
+                <div class="landing-card-info">
+                    <h1>The Cube !</h1>
+                    <p> Click Me !</p>
+                </div>
+            </button>
+        </div>
+
+        <div class="logo-container">
+            <?php foreach ($logos as $logo): ?>
+                <div class="logo-item">
+                    <div class="logo-item-info">
+                    <h2><?php echo htmlspecialchars($logo['title']); ?></h2>
+                    <img src="<?php echo htmlspecialchars($logo['image']); ?>" alt="" srcset=""></div>
+                </div>
+            <?php endforeach; ?>
+        </div>
     </main>
     
 </body>
